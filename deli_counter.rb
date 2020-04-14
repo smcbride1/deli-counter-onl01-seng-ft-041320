531a2fbf6ab57845d627f43239439dfa82d215f2
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 21:19:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.size > 0
     formatted_string = ""
     katz_deli.each_with_index do | name, index |
-      formatted_string = formatted_string + "#{index}. #{name} "
+      formatted_string = formatted_string + "#{index}. #{name}"
     "The line is currently: "
   else
 
