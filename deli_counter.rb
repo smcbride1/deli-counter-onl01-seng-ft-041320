6ee48836e10687e548cfafd0ff573573bb434dd9
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 21:34:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
     formatted_string = formatted_string[0...-1]
     "The line is currently: #{formatted_string}"
   else
-  "The line is currently empty."
+  puts "The line is currently empty."
   end
 end
 
