45f59467c8bcae67a6f0737a657cc4265a2841f3
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 21:32:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,6 @@
 # Write your code here.
+katz_deli = []
+
 def line(katz_deli)
   if katz_deli.size > 0
     formatted_string = ""
@@ -9,4 +11,18 @@ def line(katz_deli)
     "The line is currently: #{formatted_string}"
   else
   "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.last} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size > 0
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
 end
