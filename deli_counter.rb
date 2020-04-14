8d682697b84f0478ca0cfb5316f38d6391f38502
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 21:18:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.size > 0
+    formatted_string = ""
+    katz_deli.each_with_index do | name, index |
+      formatted_string = formatted_string + "#{index}. #{name} "
+    "The line is currently: "
+  else
+
+  "The line is currently:"
+  "The line is currently empty."
+end
